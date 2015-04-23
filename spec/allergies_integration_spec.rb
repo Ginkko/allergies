1f213f70bab2703e594cdfad817ecc0d5c7afa5d
Toby Alden & Alec Arme <student@epicodus.com>
2015-04-23 15:40:44 -0700

    Complete Gemfile. Adds integration spec, does not pass - missing views and app code.

---
+++ spec/allergies_integration_spec.rb
@@ -0,0 +1,15 @@
+require('capybara/rspec')
+require('capybara')
+require('/lib/allergies')
+
+Capybara.app = Sinatra::Application
+set(:show_expectations,false)
+
+describe('The path to the allergies page', {:type => :feature}) do
+  it('Presents a form to input a number with a submit button to take the user to a page displaying thier allergies.') do
+    visit('/')
+    fill_in('allergy_score', :with => '129')
+    click_button('submit')
+    expect(page).to(have_content('You are allergic to: eggs and cats.'))
+  end
+end
